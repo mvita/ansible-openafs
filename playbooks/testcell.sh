411bb51c0483b0c08d8568d324de17cc3340b3d2
Michael Meffie <mmeffie@sinenomine.net>
2019-11-11 10:43:29 -0500

    WIP

--- playbooks/testcell.sh
+++ playbooks/testcell.sh
@@ -2,10 +2,12 @@
 set -e
 #set -x
 
-echo "Running playbooks for ${VIRTLAB_NAME}"
-ansible-playbook -i ${VIRTLAB_SCRIPTDIR}/inventory.sh ${VIRTLAB_LOCAL_PLAYBOOKS} ${VIRTLAB_PLAYBOOK}
+test "${VIRTLAB_VERBOSE}" = "yes" && VERBOSE="-v"
 
-if [ ! -z "${VIRTLAB_TESTSUITE}"]; then
+echo "Running playbooks for ${VIRTLAB_NAME}"
+ansible-playbook ${VERBOSE} -i ${VIRTLAB_SCRIPTDIR}/inventory.sh ${VIRTLAB_LOCAL_PLAYBOOKS} ${VIRTLAB_PLAYBOOK}
+
+if [ ! -z "${VIRTLAB_TESTSUITE}" ]; then
     echo "Running test suite ${VIRTLAB_TESTSUITE}"
     ssh ${VIRTLAB_TESTHOST} run-openafs-robotest.sh ${VIRTLAB_TESTSUITE}
 fi
